a8fac0a59b55683403c72172d4bcf2f2838cc976
phmcastro702 <phm.castro@hotmail.com>
2022-08-11 11:14:35 -0300

    nova url de backend + import melhor para fonte global

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,11 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;700&display=swap');
-
+    
     body {
         font-family: 'Poppins', sans-serif;
         font-size: 16px;
+        font-weight: 400;
     }
 
 `;
